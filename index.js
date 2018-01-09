22e449881023fb6fe4c0189942d0c3f5d989759f
bardleware <devan.sisson@vivintsolar.com>
2018-01-09 10:32:21 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Constants
+const backgroundColor = `#040404`;
+const foregroundColor = '#00afff';
+const borderColor = '#205375';
+
+const colors = {
+  black: '#000000',
+  red: '#f44336',
+  green: '#84bf40',
+  yellow: '#ffaf5f',
+  blue: '#03a9f4',
+  magenta: '#e92663',
+  cyan: '#00bcd4',
+  white: '#aaaaaa',
+  lightblack: '#686868',
+  lightRed: '#f66055',
+  lightGreen: '#9ccc66',
+  lightYellow: '#ffbf80',
+  lightBlue: '#81d4fa',
+  lightMagenta: '#f48fb1',
+  lightCyan: '#80deea',
+  lightWhite: '#dddddd'
+};
+
+// Theme config
+exports.decorateConfig = (config) => (
+  Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    borderColor: borderColor,
+    cursorColor: foregroundColor,
+    cursorShape: 'UNDERLINE',
+    fontFamily: '"Meslo LG L", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    colors,
+    css: `
+      ${config.css || ''}
+    `
+  })
+);
